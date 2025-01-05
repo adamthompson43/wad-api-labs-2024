10c4aac94ca8cb824b202ba08cc31fd26d0471f9
Adam T <aovert.at@gmail.com>
2025-01-05 03:59:24 +0000

    express lab - exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -20,14 +20,17 @@ router.get('/:id', (req, res) => {
 
 //Add a task
 router.post('/', (req, res) => {
-    const { title, description, deadline, priority, done } = req.body;
+    const { title, description, deadline, priority, done, created_at, updated_at } = req.body;
+    const now = new Date().toISOString()
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at: now,
+        updated_at: now
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -41,7 +44,8 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const now = new Date().toISOString();
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, updated_at: now };
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
@@ -56,5 +60,6 @@ router.delete('/:id', (req, res) => {
     res.status(204).send();
     tasksData.total_results--;
 });
+
 
 export default router;
--- tasky-api/api/tasks/tasksData.js
+++ tasky-api/api/tasks/tasksData.js
@@ -4,5 +4,5 @@ export const tasksData = {
     total_results: 3,
     tasks: [{ id: "1", title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority: "low" },
     { id: "2", title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority: "medium" },
-    { id: "3", title: "Tidy up", deadline: "Today", done: false, priority: "high" }]
+    { id: "3", title: "Tidy up", deadline: "Today", done: false, priority: "high", }]
 };
